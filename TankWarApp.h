0da4578179c28a7ff46466c6af927416481f1552
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-11-06 20:58:07 +0800

    Upload new file

---
+++ TankWarApp.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <windows.h>
+#include <tchar.h>
+#include"GraphicUnit.h"
+#include"GameData.h"
+#include"Map.h"
+#include"MapData.h"
+#include"Tank.h"
+#include"TankData.h"
+#include"Control.h"
+#include"GameAI.h"

88d4cc3e39152d43542a10d6b3ff738d802f34d4
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-11-11 10:02:50 +0800

    Replace Tank.cpp

--- Tank.cpp
+++ Tank.cpp
@@ -1,8 +1,38 @@
 #include"Tank.h"
+#include"resource.h"
 void Tank::draw() {
+	
+	HDC hdc = GetDC(hWnd);
+	
+	
+	//
 	readData(*_tank);
-	HBRUSH brush=CreateSolidBrush(RGB(255,10,10));
-	SelectObject(hdc,brush);
-	Rectangle(hdc,450,200,490,240);
-	DeleteObject(brush);
+	
+	//
+	HBITMAP hbmp;
+	BITMAP bmp;
+	HDC hmem;
+	
+	hmem = CreateCompatibleDC(hdc);
+	hbmp = LoadBitmap(hInst,MAKEINTRESOURCE(MY_TANK));
+	SelectObject(hmem,hbmp);
+	GetObject(hbmp,sizeof(BITMAP),&bmp);
+	if(_tank->getfireflag()) {
+		RECT crect;
+		GetClientRect(hWnd, &crect);
+		int *pos = _tank->getfirepos();
+		if(pos[0]<crect.left || pos[0]>crect.right || pos[1]<crect.top || pos[1]>crect.bottom)
+			_tank->changefireflag(0);
+		else
+			Ellipse(hdc,pos[0]-2,pos[1]-2,pos[0]+2,pos[1]+2);
+	}
+	RECT myrect;
+	myrect.bottom = _tank->getposy()+10;
+	myrect.left = _tank->getposx()-10;
+	myrect.right = _tank->getposx()+10;
+	myrect.top = _tank->getposy()-10;
+	StretchBlt(hdc,myrect.left,myrect.top,myrect.right-myrect.left,myrect.bottom-myrect.top,
+		hmem,0,0,bmp.bmWidth,bmp.bmHeight,SRCCOPY);
+	DeleteObject(hbmp);
+	ReleaseDC(hWnd,hdc);
 }

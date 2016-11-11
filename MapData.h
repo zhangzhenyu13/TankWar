f43ca36f62ab6661696dbc1656f10c7c3f1e8880
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-11-11 10:01:30 +0800

    Replace MapData.h

--- MapData.h
+++ MapData.h
@@ -1,10 +1,29 @@
 #pragma once
+#include<vector>
 #include"GameData.h"
-class MapData :public GameData {
-	MapData* _map;
+#include<windows.h>
+struct Brick{
+	RECT rect;
+	char style;//1:normal,2:bonus,3:iron
+};
+class MapData1 :public GameData {
+	MapData1* _map;
+	std::vector<Brick> data;
+protected:
+	virtual void generateMap();
 public:
 	virtual GameData* ReadData() {
 		return _map;
 	}
+	MapData1() { generateMap(); }
 	virtual void WriteData(GameData&) {}
+	int size() { return data.size(); }
+	Brick getPos(int i) {
+		
+		return data[i];
+	
+	}
+};
+class MapData2 :public MapData1 {
+
 };

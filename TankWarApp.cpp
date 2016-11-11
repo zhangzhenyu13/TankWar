d220042bbbc318292f2a4c70c820e14c8570c195
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-11-11 10:04:25 +0800

    Replace TankWarApp.cpp

--- TankWarApp.cpp
+++ TankWarApp.cpp
@@ -1,4 +1,5 @@
 #include"TankWarApp.h"
+#include"resource.h"
 /*
 GameSketch
 720p screen utility
@@ -6,9 +7,32 @@ GameInput-->GameAIHandle-->GameDraw
 in WndProc as
 case WM_PAINT, case WM_KEYDOWN
 */
+static HINSTANCE hInst;
+static Control* myInput;
+static GameData* myTankData;
+static GameData* myData;
+static GameAI* myAI;
 
+BOOLEAN InitData(HWND& hWnd) {
+	myData = new MapData1();
+	myTankData = new TankData();
+	return TRUE;
+}
+
+BOOLEAN InitControl(HWND& hWnd) {
+
+	return TRUE;
+}
+BOOLEAN Release() {
+
+	delete myInput;
+	delete myAI;
+	delete myData;
+	return TRUE;
+}
 BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOLEAN CALLBACK MYDlg(HWND, UINT, WPARAM, LPARAM);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	MSG msg;
@@ -22,44 +46,79 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+	Release();
 	return (int)msg.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	Control*user=new Control;
-	Map *mymap;
-	Tank* mytank;
-	GameAI *App=new GameAI;
-	HBRUSH brush = CreateSolidBrush(RGB(100, 100, 100));
+	
+	PAINTSTRUCT ps;
+	HDC hdc;
+	HBRUSH brush;
+
+	myInput = new Control;
+	Tank myTank(hInst,hWnd);
+	Map myMap(hInst,hWnd);
+	myMap.readData(*myData);
+	myTank.readData(*myTankData);
+	
+	TankData* mytankdata = myTank.getData();
+
+
 	switch (message)
 	{
-
+	case WM_CREATE:
+		SetTimer(hWnd,1,1,NULL);
+		break;
 	case WM_PAINT:
-		HDC hdc;
-		PAINTSTRUCT ps;
-		hdc = BeginPaint(hWnd, &ps);
-		//entrance for painting code
+		hdc=BeginPaint(hWnd,&ps);
+
+		//myMap.draw();
+		myTank.draw();
+		if(mytankdata->getfireflag()) {
+			int *pos = mytankdata->getfirepos();
+			switch (mytankdata->getfiredirect())
+			{
+			case 'u':
+				mytankdata->writeposfire(pos[0],pos[1]-5);
+				break;
+			case 'l':
+				mytankdata->writeposfire(pos[0]-5,pos[1]);
+				break;
+			case 'r':
+				mytankdata->writeposfire(pos[0]+5,pos[1]);
+				break;
+			case 'd':
+				mytankdata->writeposfire(pos[0],pos[1]+5);
+				break;
+			default:
+				break;
+			}
+		}
 		
-		mytank = new Tank(hdc);
-		mymap = new Map(hdc);
-		mymap->draw();
-		mytank->draw();
-		delete mymap;
-		//delete others
 		EndPaint(hWnd,&ps);
 		break;
 	case WM_KEYDOWN:
 		//Game input data and handle
-		user->move();
-		user->fire();
-		App->Handle();
+		//DialogBoxW(hInst,MAKEINTRESOURCE(MY_DIALOG),hWnd,(DLGPROC)MYDlg);
+
+		//press direction key to move
+		myInput->move(wParam, mytankdata);
+		//press SPACE to fire
+		if(wParam == VK_SPACE)
+			myInput->fire(mytankdata);
+		InvalidateRect(hWnd,NULL,1);
+		break;
+	case WM_TIMER:
+		InvalidateRect(hWnd,NULL,1);
 		break;
 	case WM_DESTROY:
-		PostQuitMessage(0);															//调用PostQuitMessage发出WM_QUIT消息
+		PostQuitMessage(0);
+		KillTimer(hWnd,1);
 		break;
 	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);						//默认时采用系统消息默认处理函数
+		return DefWindowProc(hWnd, message, wParam, lParam);
 		break;
 	}
 	return 0;
@@ -78,8 +137,8 @@ BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow)
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wcex.lpszMenuName = NULL;
+	wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcex.lpszMenuName = MAKEINTRESOURCE(MY_MENU);
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
 	if (!RegisterClassEx(&wcex))
@@ -98,7 +157,31 @@ BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow)
 
 	if (!hWnd)
 		return FALSE;
+	//
+	hInst = hInstance;
+	InitData(hWnd);
+
+	InitControl(hWnd);
+
+	//
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
+	return TRUE;
+}
+
+BOOLEAN CALLBACK MYDlg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			break;
+		}
+		break;
+	}
+	
 	return TRUE;
 }

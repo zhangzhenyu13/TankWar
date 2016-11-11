61783f5ef95c9628c43af7ff0745bad7b7dd5f07
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-11-11 09:59:41 +0800

    Replace Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -1,12 +1,38 @@
 #include"Map.h"
-#include<cstdlib>
+#include<windows.h>
+#include"resource.h"
 void Map::draw() {
-	readData(*_map);
-	HBRUSH brush=CreateSolidBrush(RGB(100,200,100));
-	SelectObject(hdc,brush);
-	int x, y;
-	for (y = 0; y < 14; y++)
-		for (x = 0; x < 24; x++)
-			rand() % 2 == 1 ? Rectangle(hdc, x * 50, y * 50, x * 50 + 50, y * 50 + 50) :1;
-	DeleteObject(brush);
+	HDC hdc = GetDC(hWnd);
+
+	//
+	
+	//
+	HBITMAP hbmp,hbmp1;
+	BITMAP bmp,bmp1;
+	HDC hmem,hmem1;
+
+	hmem = CreateCompatibleDC(hdc);
+	hbmp = LoadBitmap(hInst, MAKEINTRESOURCE(NORMAL_BRICK));
+	hbmp1 = LoadBitmap(hInst, MAKEINTRESOURCE(BONUS_BRICK));
+	
+	GetObject(hbmp1,sizeof(BITMAP),&bmp1);
+	GetObject(hbmp, sizeof(BITMAP), &bmp);
+	RECT crect;
+	GetClientRect(hWnd, &crect);
+	int x=1, y=1;
+	RECT rect;
+	SelectObject(hmem, hbmp1);
+	
+	for (int i = 0; i < _map->size(); i++) {
+		if (_map->getPos(i).style == 1) {
+			SelectObject(hmem,hbmp);
+			rect = _map->getPos(i).rect;
+			StretchBlt(hdc, rect.left, rect.top, rect.right-rect.left, rect.bottom-rect.top,
+				hmem, 0, 0, bmp.bmWidth, bmp.bmHeight, SRCCOPY);
+		}
+		
+	}
+	DeleteObject(hbmp);
+	DeleteObject(hbmp1);
+	ReleaseDC(hWnd, hdc);
 }

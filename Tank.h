b3bff41de5b15064c4d209d06d6bac8baf00467d
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-11-11 10:03:20 +0800

    Replace Tank.h

--- Tank.h
+++ Tank.h
@@ -3,11 +3,21 @@
 #include"GraphicUnit.h"
 #include"TankData.h"
 class Tank:public GraphicUnit{
-	HDC hdc;
+	HWND& hWnd;
+	HINSTANCE &hInst;
 	TankData* _tank;
 public:
-	Tank(HDC& hdc1):hdc(hdc1) {}
+	Tank(HINSTANCE& hInst1,HWND& hWnd1):hWnd(hWnd1),hInst(hInst1){
+
+	}
+
+	TankData* getData() {
+		return _tank;
+	}
+	
 	virtual void draw();
-protected:
-	virtual void readData(GameData&) {}
+
+	virtual void readData(GameData& newtank) {
+		_tank = (TankData*)&newtank;
+	}
 };

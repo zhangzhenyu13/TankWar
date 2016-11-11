9fc5eb4a6d4af856abafd4acfd56e128176ecbd3
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-11-11 10:00:45 +0800

    Replace MapData.cpp

--- MapData.cpp
+++ MapData.cpp
@@ -1,1 +1,16 @@
 #include"MapData.h"
+using namespace std;
+void MapData1::generateMap() {
+	Brick brick;
+	
+	for(int i=0;i<6;i++)
+		for (int j = 0; j < 12; j++) {
+			brick.style = 1;
+			//brick.rect = {60+i*20,60+j*20,60+i*20+20,60+j*20+20};
+			brick.rect.bottom = 60+j*20+20;
+			brick.rect.left = 60+i*20;
+			brick.rect.right = 60+i*20+20;
+			brick.rect.top = 60+j*20;
+			data.push_back(brick);
+		}
+}

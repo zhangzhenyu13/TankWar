d8d582f805ba44bfe0bf73f78fa06722d04b6991
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-11-18 14:18:58 +0800

    Replace TankWarApp.h

--- TankWarApp.h
+++ TankWarApp.h
@@ -1,11 +1,19 @@
 #pragma once
 #include <windows.h>
 #include <tchar.h>
-#include"GraphicUnit.h"
+#include<vector>
+#include"resource.h"
+#include"UnitID.h"
+//
 #include"GameData.h"
-#include"Map.h"
-#include"MapData.h"
-#include"Tank.h"
 #include"TankData.h"
+#include"MapData.h"
+
+#include"GraphicLayout.h"
+#include"GraphicUnit.h"
+#include"Map.h"
+#include"Tank.h"
+
 #include"Control.h"
 #include"GameAI.h"
+

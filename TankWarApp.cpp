9fa482620c168bbde79fd943063d8f218d7f90ed
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-11-18 14:18:43 +0800

    Replace TankWarApp.cpp

--- TankWarApp.cpp
+++ TankWarApp.cpp
@@ -1,38 +1,64 @@
 #include"TankWarApp.h"
-#include"resource.h"
+
 /*
-GameSketch
+
+GameSketch : MCV
 720p screen utility
 GameInput-->GameAIHandle-->GameDraw
 in WndProc as
 case WM_PAINT, case WM_KEYDOWN
 */
+ULONG_PTR m_gdiplusToken;
+
 static HINSTANCE hInst;
-static Control* myInput;
-static GameData* myTankData;
-static GameData* myData;
-static GameAI* myAI;
-
+static Control* Input;
+static TankData* tankData;
+static MapData* mapData;
+static GameAI* RuleAI;
+static GraphicUnit *tank, *map;
+static GraphicLayout* Layout;
 BOOLEAN InitData(HWND& hWnd) {
-	myData = new MapData1();
-	myTankData = new TankData();
+//M&C Init
+	Input = new Control();
+	mapData = new MapData1();
+	tankData = new TankData();
+	RuleAI = new GameAI();
+	RuleAI->addMap(mapData);
+	RuleAI->addTank(tankData);
+	RuleAI->addControl(Input);
+//V Init
+	Layout = new GraphicLayout(hWnd,hInst);
+	tank = new Tank();
+	map = new Map();
+	tank->readData(tankData);
+	map->readData(mapData);
+	Layout->addGraphicUnit(map);
+	Layout->addGraphicUnit(tank);
+	Layout->Init();
+
+	RuleAI->setGraph(Layout);
+//
+     Gdiplus::GdiplusStartupInput GdiplusStartupInput;
+     Gdiplus::GdiplusStartup(&m_gdiplusToken, &GdiplusStartupInput, NULL);
 	return TRUE;
 }
 
-BOOLEAN InitControl(HWND& hWnd) {
 
-	return TRUE;
-}
 BOOLEAN Release() {
-
-	delete myInput;
-	delete myAI;
-	delete myData;
+	Gdiplus::GdiplusShutdown(m_gdiplusToken);
+	//releasedata
+	delete Input;
+	delete RuleAI;
+	delete mapData;
+	delete tankData;
+	delete Layout;
+	delete tank;
+	delete map;
 	return TRUE;
 }
 BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-BOOLEAN CALLBACK MYDlg(HWND, UINT, WPARAM, LPARAM);
+BOOLEAN CALLBACK SelectModeProc(HWND, UINT, WPARAM, LPARAM);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	MSG msg;
@@ -53,65 +79,44 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	
-	PAINTSTRUCT ps;
-	HDC hdc;
-	HBRUSH brush;
-
-	myInput = new Control;
-	Tank myTank(hInst,hWnd);
-	Map myMap(hInst,hWnd);
-	myMap.readData(*myData);
-	myTank.readData(*myTankData);
-	
-	TankData* mytankdata = myTank.getData();
-
-
 	switch (message)
 	{
 	case WM_CREATE:
-		SetTimer(hWnd,1,1,NULL);
+		SetTimer(hWnd,1,10,NULL);
 		break;
 	case WM_PAINT:
-		hdc=BeginPaint(hWnd,&ps);
-
-		//myMap.draw();
-		myTank.draw();
-		if(mytankdata->getfireflag()) {
-			int *pos = mytankdata->getfirepos();
-			switch (mytankdata->getfiredirect())
-			{
-			case 'u':
-				mytankdata->writeposfire(pos[0],pos[1]-5);
-				break;
-			case 'l':
-				mytankdata->writeposfire(pos[0]-5,pos[1]);
-				break;
-			case 'r':
-				mytankdata->writeposfire(pos[0]+5,pos[1]);
-				break;
-			case 'd':
-				mytankdata->writeposfire(pos[0],pos[1]+5);
-				break;
-			default:
-				break;
-			}
-		}
-		
-		EndPaint(hWnd,&ps);
+		Layout->RefreshWnd(hWnd);
+		Layout->Init();
+		Layout->draw();
 		break;
 	case WM_KEYDOWN:
-		//Game input data and handle
-		//DialogBoxW(hInst,MAKEINTRESOURCE(MY_DIALOG),hWnd,(DLGPROC)MYDlg);
-
-		//press direction key to move
-		myInput->move(wParam, mytankdata);
-		//press SPACE to fire
-		if(wParam == VK_SPACE)
-			myInput->fire(mytankdata);
-		InvalidateRect(hWnd,NULL,1);
+		Input->Input(wParam);
+		RuleAI->Handle();
+		InvalidateRect(hWnd,NULL,FALSE);
 		break;
+	case WM_KEYUP:
+		Input->keep();
+		RuleAI->Handle();
+		InvalidateRect(hWnd, NULL, FALSE);
 	case WM_TIMER:
-		InvalidateRect(hWnd,NULL,1);
+		//flash
+		RuleAI->Handle(true);
+		InvalidateRect(hWnd,NULL,FALSE);
+		break;
+	
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case GAME_EXIT:
+			PostQuitMessage(0);
+			KillTimer(hWnd, 1);
+			break;
+		case GAME_NEW:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_SELECTMODE), hWnd, (DLGPROC)SelectModeProc);
+			break;
+		default:
+			break;
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -137,7 +142,7 @@ BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow)
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
 	wcex.lpszMenuName = MAKEINTRESOURCE(MY_MENU);
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
@@ -146,7 +151,7 @@ BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow)
 	hWnd = CreateWindowW(
 		szWindowClass,
 		szTitle,
-		WS_OVERLAPPEDWINDOW,
+		WS_CAPTION,
 		0, 0,
 		1208, 720,//720p gameGraphic
 		NULL,
@@ -161,7 +166,7 @@ BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow)
 	hInst = hInstance;
 	InitData(hWnd);
 
-	InitControl(hWnd);
+	
 
 	//
 	ShowWindow(hWnd, nCmdShow);
@@ -169,7 +174,7 @@ BOOLEAN InitWindowClass(HINSTANCE hInstance, int nCmdShow)
 	return TRUE;
 }
 
-BOOLEAN CALLBACK MYDlg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+BOOLEAN CALLBACK SelectModeProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
@@ -177,9 +182,15 @@ BOOLEAN CALLBACK MYDlg(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 		case IDOK:
 			DestroyWindow(hWnd);
 			break;
-		default:
+		case IDCANCEL:
 			break;
 		}
+		break;
+	case WM_INITDIALOG:
+		SetWindowText(hWnd,L"SELECT GAME MODE");
+		break;
+	default:
+		DefWindowProc(hWnd, message, wParam, lParam);
 		break;
 	}
 	

a6f3d9780d3edfc5e217e98009b7849f0eacd3d8
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-11-18 14:17:14 +0800

    Replace Tank.h

--- Tank.h
+++ Tank.h
@@ -3,21 +3,27 @@
 #include"GraphicUnit.h"
 #include"TankData.h"
 class Tank:public GraphicUnit{
-	HWND& hWnd;
-	HINSTANCE &hInst;
+	HWND* pWnd;
+	HINSTANCE *pInst;
+	HDC* pDC,*pDCObj;
 	TankData* _tank;
+	std::string tankname;
 public:
-	Tank(HINSTANCE& hInst1,HWND& hWnd1):hWnd(hWnd1),hInst(hInst1){
-
+	void setHandle(HWND* hWnd1,HINSTANCE* hInst1){
+		this->pInst = hInst1;
+		this->pWnd = hWnd1;
 	}
-
-	TankData* getData() {
-		return _tank;
+	void setDC(HDC* hdc1,HDC*hdc2) {
+		pDC = hdc1;
+		pDCObj = hdc2;
 	}
 	
+	std::string getName() {
+		return tankname;
+	}
 	virtual void draw();
 
-	virtual void readData(GameData& newtank) {
-		_tank = (TankData*)&newtank;
+	virtual void readData(GameData* newtank) {
+		_tank = dynamic_cast<TankData*>(newtank);
 	}
 };

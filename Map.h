5e3c3f879d19219291038684fcf8341dc807ca6b
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-11-18 14:15:53 +0800

    Replace Map.h

--- Map.h
+++ Map.h
@@ -1,16 +1,28 @@
 #pragma once
+#include<windows.h>
+#include"GameData.h"
 #include"GraphicUnit.h"
 #include"MapData.h"
-#include"windows.h"
-class Map :public GraphicUnit{
-	HWND& hWnd;
-	HINSTANCE& hInst;
-	MapData1* _map;
-public:
-	Map(HINSTANCE& hInst1,HWND& hWnd1):hWnd(hWnd1),hInst(hInst1) {}
-	virtual void draw();
 
-	virtual void readData(GameData& _map1) {
-		_map = (MapData1*)&_map1;
+class Map :public GraphicUnit{
+	HWND* pWnd;
+	HINSTANCE* pInst;
+	HDC* pDC,*pDCObj;
+	MapData* _map;
+	std::string mapname;
+public:
+	
+	void setHandle(HWND*hWnd,HINSTANCE*hInst) {
+		this->pWnd = hWnd;
+		this->pInst = hInst;
+	}
+	void setDC(HDC* hdc1,HDC* hdc2) {
+		pDC = hdc1;
+		pDCObj = hdc2;
+	}
+	virtual void draw();
+	std::string getName() { return mapname; }
+	virtual void readData(GameData* map) {
+		_map = dynamic_cast<MapData*>(map);
 	}
 };

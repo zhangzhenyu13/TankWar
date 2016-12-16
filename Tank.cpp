0ac961a4f4ac1f4577c12c340d5342a4b2bb3ea8
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-12-16 09:35:53 +0800

    Replace Tank.cpp

--- Tank.cpp
+++ Tank.cpp
@@ -3,31 +3,42 @@
 using namespace Gdiplus;
 using namespace std;
 void Tank::draw() {
-	string s;
-	LPWSTR up = L"tank\\tank_up.png", down = L"tank\\tank_down.png",
-		left = L"tank\\tank_left.png", right = L"tank\\tank_right.png",pic=up;
+	if(_tank->isAlive()==false)
+		return ;
+	//paint
+	wstring tankges;
+	if(_tank->getTeam()==1)
+		tankges=L"tank\\enemy\\";
+	else
+		tankges=L"tank\\player\\";
 	
 	switch (_tank->gesDirect())
 	{
 	case UP:
-		pic = up;
+		tankges+=L"up";
 		break;
 	case DOWN:
-		pic = down;
+		tankges+=L"down";
+		
 		break;
 	case LEFT:
-		pic = left;
+		tankges+=L"left";
+		
 		break;
 	case RIGHT:
-		pic = right;
+		tankges+=L"right";
+		
 		break;
 	default:
 		break;
 	}
-
-	Image tank(pic);
+	wstring level[5]={L"1",L"2",L"3",L"4",L"5"};
+	tankges+=level[_tank->getLevel()-1];
+	tankges+=L".png";
+	//
+	LPCWSTR pic=tankges.c_str();
+	Gdiplus::Image tank(pic);
 	Graphics g(*pDCObj);
-
 	Rect rect(_tank->Xpos(),_tank->Ypos(),50,50);
 	
 	g.DrawImage(&tank, rect);

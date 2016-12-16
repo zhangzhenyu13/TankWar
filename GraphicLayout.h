f8b6a5c0a6e29af1f4363c2a98c256a87dc3b626
张振羽 <zhangzhenyu13@mails.tsinghua.edu.cn>
2016-12-16 09:34:49 +0800

    Replace GraphicLayout.h

--- GraphicLayout.h
+++ GraphicLayout.h
@@ -2,11 +2,13 @@
 #include<windows.h>
 #include<vector>
 #include"GraphicUnit.h"
+#include"StatusWnd.h"
 class GraphicLayout {
 private:
 	HWND pWnd;
 	HINSTANCE pInst;
 	std::vector<GraphicUnit*> data;
+	StatusWnd* status;
 protected:
 	
 public:
@@ -15,20 +17,29 @@ public:
 	}
 	GraphicLayout(HWND wnd, HINSTANCE inst) :pWnd(wnd), pInst(inst) {}
 	void draw();
-	
+	void drawStatus();
 	void Init() {
 		for (int i = 0; i < data.size(); i++) {
 			data[i]->setHandle(&pWnd, &pInst);
 		}
 	}
-
+	StatusWnd* Status(){return status;}
 	void addGraphicUnit(GraphicUnit* g) {
 		data.push_back(g);
 	}
+	void addStatusZone(StatusWnd* stwnd){
+		status=stwnd;
+	}
 	void deleteGraphicUnit(GraphicUnit*g) {
+		
 		std::vector<GraphicUnit*>::iterator p = find(data.begin(), data.end(), g);
 		GraphicUnit* gtmp = *p;
 		delete gtmp;
 		data.erase(p);
 	}
+	~GraphicLayout(){
+		for(int i=data.size()-1;i>=0;i--)
+			delete data[i];
+	}
+
 };

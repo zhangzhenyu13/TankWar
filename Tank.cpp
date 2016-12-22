0b34b215b7fffe8ae692ac36d9ae980abbff5021
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-12-22 23:56:50 +0800

    Replace Tank.cpp

--- Tank.cpp
+++ Tank.cpp
@@ -3,9 +3,23 @@
 using namespace Gdiplus;
 using namespace std;
 void Tank::draw() {
-	if(_tank->isAlive()==false)
-		return ;
+	if (_tank->isAlive() == false || _tank->available == false)
+		return;
 	//paint
+	int rba1 = 0, rba2 = 0;
+	if (_tank->getTeam() == 1)
+		rba1 = _tank->levelUPcooling/10;
+	else
+		rba2 = _tank->levelUPcooling/10;
+	HBRUSH bkbrush = CreateSolidBrush(RGB(10 + rba1, 10 + rba2, 30));
+	RECT rt = _tank->getPos();
+	SelectObject(*pDCObj, bkbrush);
+	if (_tank->levelUPcooling > 0) {
+	   Ellipse(*pDCObj, rt.left - 5, rt.top - 5, rt.right + 5, rt.bottom + 5);
+	   _tank->levelUPcooling-=10;
+    }
+		
+	//GdiplusImage
 	wstring tankges;
 	if(_tank->getTeam()==1)
 		tankges=L"tank\\enemy\\";
@@ -39,10 +53,10 @@ void Tank::draw() {
 	LPCWSTR pic=tankges.c_str();
 	Gdiplus::Image tank(pic);
 	Graphics g(*pDCObj);
-	Rect rect(_tank->Xpos(),_tank->Ypos(),50,50);
+	Rect rect(_tank->Xpos(),_tank->Ypos(),_tank->tankSize,_tank->tankSize);
 	
 	g.DrawImage(&tank, rect);
 
 	//release Mem
-
+	DeleteObject(bkbrush);
 }

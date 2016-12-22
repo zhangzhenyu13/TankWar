c0921bfee47a3c98f5bf9affa6650a37225c80ae
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-12-22 23:56:29 +0800

    Upload new file

---
+++ StartUpWnd.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<windows.h>
+#include"GameData.h"
+#include"GraphicUnit.h"
+class StartUpWnd :public GraphicUnit {
+	HWND* pWnd;
+	HINSTANCE* pInst;
+	HDC* pDC, *pDCObj;
+	std::string mapname;
+public:
+
+	void setHandle(HWND*hWnd, HINSTANCE*hInst) {
+		this->pWnd = hWnd;
+		this->pInst = hInst;
+	}
+	void setDC(HDC* hdc1, HDC* hdc2) {
+		pDC = hdc1;
+		pDCObj = hdc2;
+	}
+	virtual void draw();
+	std::string getName() { return mapname; }
+	virtual void readData(GameData* map) {
+		
+	}
+};

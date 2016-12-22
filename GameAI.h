9b0dfc7bfa472d6d0f3243203bef344fa545c500
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-12-22 23:50:58 +0800

    Replace GameAI.h

--- GameAI.h
+++ GameAI.h
@@ -8,23 +8,39 @@
 #include"Bullet.h"
 #include"Robot.h"
 #include<vector>
+#include<fstream>
+using namespace std;
+const int onShow = 5;
 enum GameResult
 {
 	WINNER,LOSER,COMEON
 };
 class GameAI {
 private:
-	char stage;
+	bool playerdied;
+	int playerRank;
+	char level;
 	std::vector<TankData*> tank;
-	std::vector<MapData*> map;
+	MapData* map;
 	std::vector<Control*> control;
 	std::vector<Robot*> player;
 	std::vector<Bullet*> fire;
 	GraphicLayout* glayout;
+	RECT myArea, pcArea;
 public :
-	GameAI() { stage = 1; }
+	GameAI() {level = 1; playerdied = false; playerRank = 1; }
+	Control* getPlayerInput() {
+		return control[playerRank];
+	}
 	void setGraph(GraphicLayout* g) {
 		glayout = g;
+	}
+	void setArea(RECT rect1,RECT rect2) {
+		myArea = rect1;
+		pcArea = rect2;
+	}
+	void setDifficulty(int r) {
+		level = r;
 	}
 	void addControl(Control* C) {
 		control.push_back(C);
@@ -34,7 +50,7 @@ public :
 	}
 	virtual GameResult Handle(bool timer=false);
 	void addMap(MapData* gm) {
-		map.push_back(gm);
+		map=gm;
 	}
 	void addTank(TankData* gt) {
 		tank.push_back(gt);
@@ -45,14 +61,11 @@ public :
 
 	//
 	void BeginGame();
-	void EndGame();
 	~GameAI(){
 		for(int i=tank.size()-1;i>=0;i--){
 			delete tank[i];
 		}
-		for(int i=map.size()-1;i>=0;i--){
-			delete map[i];
-		}
+			delete map;
 		for(int i=control.size()-1;i>=0;i--){
 			delete control[i];
 		}
@@ -78,23 +91,44 @@ public :
 				if (tank[j]->isAlive()&&IntersectRect(&inrect,&trect,&rect))
 					flag = true;
 			}
-		} while(map[stage-1]->interset(rect) || flag);
+		} while(map->interset(rect) || flag);
 		return p;
 	}
+	void SaveData(wstring );
 protected:
 	int countLevel(){
+		
 		StatusWnd* swnd=glayout->Status();
 		swnd->clear();
 		for(int i=0;i<tank.size();i++){
-			if(tank[i]->isAlive())
+			if(tank[i]->isAlive()&&tank[i]->available)
 			swnd->add(tank[i]->getTeam(),tank[i]->getLevel());
 		}
+		//count and add
+		int humanop = 0, myAlive = swnd->Sum(2), pcAlive = swnd->Sum(1);
+		for (int i = 0; i < tank.size(); i++) {
+			if (tank[i]->isAlive() == false||tank[i]->available)
+				continue;
+			if (tank[i]->getTeam() == 1&&pcAlive<onShow) {
+				tank[i]->available = true;
+				pcAlive++;
+			}
+			if(tank[i]->getTeam()==2&&myAlive<onShow){
+				tank[i]->available = true;
+				myAlive++;
+				humanop = i;
+			}
+		}
+		if (playerdied&&humanop > 0) {
+			playerRank = humanop;
+			player[humanop]->enabled = false;
+			playerdied = false;
+		}
 		
-		
-		if(swnd->Sum(1)==0)
-			return 1;
-		else if(swnd->Sum(2)==0)
-			return 2;
+		if(pcAlive==0)
+			return 1;//pc
+		else if(myAlive==0)
+			return 2;//my
 		else
 			return 0;
 

c4b98a51635179f1ce1aa72c1bac5ff8cd4a5e5e
张朔文 <zhangsw13@mails.tsinghua.edu.cn>
2016-12-22 23:50:00 +0800

    Replace CommonFunc.h

--- CommonFunc.h
+++ CommonFunc.h
@@ -7,29 +7,6 @@ namespace DataFunc {
 			for (int x = rect.left; x <= rect.right; x++)
 				map[y][x] = flag;
 	}
-	bool IsaPoint(POINT p, RECT rect) {
-		if (p.x<rect.right&&p.x>rect.left)
-			if (p.y<rect.bottom&&p.y>rect.top)
-				return true;
-			else
-				return false;
-	}
-	bool interset(RECT& rect1,RECT& rect2) {
-		int x, y;
-		x = rect1.left; y = rect1.top;
-		if (IsaPoint({ x,y }, rect2))
-			return true;
-		x = rect1.right;
-		if (IsaPoint({ x,y }, rect2))
-			return true;
-		y = rect1.bottom;
-		if (IsaPoint({ x,y }, rect2))
-			return true;
-		x = rect1.left;
-		if (IsaPoint({ x,y }, rect2))
-			return true;
-		return false;
-		
-	}
+	
 	
 };
